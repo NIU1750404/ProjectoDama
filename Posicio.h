3b438146552475975f678458c81643e1fb89818c
Oscar Perez <1750404@uab.cat>
2025-04-09 16:32:08 +0200

    posicio.h

--- Posicio.h
+++ Posicio.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef POSICIO_H
+#define POSICIO_H
+
+
+class Posicio
+{
+public:
+
+	Posicio() : m_fila(0), m_columna(0) {}
+	Posicio(int fila, int columna) : m_fila(fila), m_columna(columna) {};
+
+	int getFila() const { return m_fila; };
+	int getColumna() const { return m_columna; };
+
+	void setFila(int fila) { m_fila = fila; }
+	void setColumna(int columna) { m_columna = columna; }
+
+private:
+	int m_fila;
+	int m_columna;
+};
+
+
+#endif
