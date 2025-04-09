5e3554489aaba2dffe05433e67bbef4d3e9d8adc
Ali <AliSaad.Ullah@autonoma.cat>
2025-04-10 01:31:57 +0200

    m

--- Moviment.h
+++ Moviment.h
@@ -1,1 +1,29 @@
+#ifndef MOVIMENT.H
+#define MOVIMENT.H
+#include <string>
+#include "Posicio.h"
+
 using namespace std;
+
+class Moviment
+{
+public:
+	Moviment() : m_inicial(Posicio()), m_final(Posicio()) {}
+	Moviment(const Posicio& inicial, const Posicio & final) : m_inicial(inicial), m_final(final) {}
+
+	Posicio getInicial() const { return m_inicial; }
+	Posicio getFinal() const { return m_final; }
+	void setInicial(const Posicio& inicial) { m_inicial = inicial; }
+	void setFinal(const Posicio & final) { m_final = final; }
+
+
+	string toString() const;
+
+private:
+	Posicio m_inicial;
+	Posicio m_final;
+
+};
+
+
+#endif
