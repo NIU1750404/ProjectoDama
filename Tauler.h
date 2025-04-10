3e0d9b73e951f959822ddd0ba25971614d32dc8c
Ali <AliSaad.Ullah@autonoma.cat>
2025-04-10 13:44:55 +0200

    t

--- Tauler.h
+++ Tauler.h
@@ -1,1 +1,34 @@
-#pragma once
+#ifndef TAULER_H
+#define TAULER_H
+#include <string>
+#include "Posicio.h"
+#include "Moviment.h"
+#include "Fitxa.h"
+
+using namespace std;
+
+static const int N_FILES = 8;
+static const int N_COLUMNES = 8;
+
+class Tauler
+{
+public:
+	Tauler();
+	~Tauler();
+	Tauler(const Tauler& tauler);
+
+	void inicialitza(const string& nomFitxer);
+	void actualitzaMovimentsValids();
+	bool mouFitxa(const Posicio& origen, const Posicio& desti);
+	void getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]);
+
+	string toString() const;
+private:
+	Fitxa m_tauler[N_FILES][N_COLUMNES];
+	int m_torn;
+
+};
+
+
+#endif
+
