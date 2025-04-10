0348990cf831cfd7af8c3bd3d796407bce9b171b
Oscar Perez <oscarperezcanet@gmail.com>
2025-04-10 17:17:01 +0200

    fitxa

--- Fitxa.h
+++ Fitxa.h
@@ -1,8 +1,9 @@
 #ifndef FITXA_H
 #define FITXA_H
-
+#include "Posicio.h"
 using namespace std;
 #include <string>
+static const int MAX_MOVIMENTS_POSIBLES = 4;
 
 typedef enum
 {
@@ -21,23 +22,25 @@ typedef enum
 class Fitxa {
 public:
     
-    Fitxa() : m_tipus(TIPUS_EMPTY), m_color(COLOR_NEGRE), m_posicio(0,0) {}
+    Fitxa() : m_tipus(TIPUS_EMPTY), m_color(COLOR_NEGRE){}
     Fitxa(TipusFitxa tipus, ColorFitxa color, Posicio posicio) : m_tipus(tipus), m_color(color), m_posicio(posicio) {}
 
     
     TipusFitxa getTipus() const { return m_tipus; }
     ColorFitxa getColor() const { return m_color; }
     Posicio getPosicio() const { return m_posicio; }
+    Posicio getPosicioPosible(int posicio) const { return m_posicionsPosibles[posicio]; }
     void setTipus(TipusFitxa tipus) { m_tipus = tipus; }
     void setColor(ColorFitxa color) { m_color = color; }
     void setPosicio(Posicio posicio) { m_posicio = posicio; }
+    void setPosicioPosibles(Posicio posicio, int i) { m_posicionsPosibles[i] = posicio; }
 
     bool esBuida() const { return m_tipus == TIPUS_EMPTY; }
     bool esBlanca() const { return m_color == COLOR_BLANC && m_tipus != TIPUS_EMPTY; }
     bool esNegra() const { return m_color == COLOR_NEGRE && m_tipus != TIPUS_EMPTY; }
     bool esDama() const { return m_tipus == TIPUS_DAMA; }
 	bool esNormal() const { return m_tipus == TIPUS_NORMAL; }
-    bool potMenjar();
+    
 
 
 
@@ -45,5 +48,6 @@ private:
     TipusFitxa m_tipus;  
     ColorFitxa m_color;  
     Posicio m_posicio;
+    Posicio m_posicionsPosibles[MAX_MOVIMENTS_POSIBLES];
 };
 #endif
