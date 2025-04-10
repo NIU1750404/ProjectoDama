4b72ccc2b8492d45cf6f372a5447ddc81238bfbc
Ali <AliSaad.Ullah@autonoma.cat>
2025-04-10 12:48:07 +0200

    mov

--- Moviment.h
+++ Moviment.h
@@ -1,5 +1,5 @@
-#ifndef MOVIMENT.H
-#define MOVIMENT.H
+#ifndef MOVIMENT_H
+#define MOVIMENT_H
 #include <string>
 #include "Posicio.h"
 
@@ -8,20 +8,26 @@ using namespace std;
 class Moviment
 {
 public:
-	Moviment() : m_inicial(Posicio()), m_final(Posicio()) {}
-	Moviment(const Posicio& inicial, const Posicio & final) : m_inicial(inicial), m_final(final) {}
+	Moviment() : m_inicial(Posicio()), m_final(Posicio()), m_nCaptures(0), m_nPosicionsMitjanes(0) {}
+	Moviment(const Posicio& inicial, const Posicio& final) : m_inicial(inicial), m_final(final), m_nCaptures(0), m_nPosicionsMitjanes(0) {}
 
 	Posicio getInicial() const { return m_inicial; }
 	Posicio getFinal() const { return m_final; }
 	void setInicial(const Posicio& inicial) { m_inicial = inicial; }
 	void setFinal(const Posicio & final) { m_final = final; }
 
+	int getNCaptures() const { return m_nCaptures; }
+	int getNPosicionsMitjanes() const { return m_nPosicionsMitjanes; }
+	void setNCaptures(int nCaptures) { m_nCaptures = nCaptures; }
+	void setNPosicionsMitjanes(int nPosicionsMitjanes) { m_nPosicionsMitjanes = nPosicionsMitjanes; }
 
 	string toString() const;
 
 private:
 	Posicio m_inicial;
 	Posicio m_final;
+	int m_nCaptures;
+	int m_nPosicionsMitjanes;
 
 };
 
